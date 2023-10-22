b175fdc7addb0107d4225681aa98532f034715f0
Abdullah Alawad <abdullahalawad123321@gmail.com>
2023-10-22 21:12:19 +0300

    Tests are Done

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+  const itemClass = inCart? "in-cart" : "";
+
+  function switchCart(){
+    setInCart((inCart) => !inCart);
+  }
+
+  const switchCarts = inCart? "Remove From Cart" : "Add to Cart";
+
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={switchCart}>{switchCarts}</button>
     </li>
   );
 }
